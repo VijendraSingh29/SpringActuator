85552542d94e8d0af93851aec88cb18d00b1fa44
Vijendra <vijendra.jadaun@gmail.com>
2023-12-26 13:23:40 +0530

    added spring acutator dependencies, added custom end points,/health - health indicator is modified with the service

---
+++ src/main/java/com/POC/Actuator/CustomHealthEndPoints/HealthIndicatorService.java
@@ -0,0 +1,27 @@
+package com.POC.Actuator.CustomHealthEndPoints;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HealthIndicatorService implements HealthIndicator {
+    private final String message_key = "Service A";
+
+    @Override
+    public Health health() {
+        if(!isRunningServiceA())
+        {
+            return Health.down().withDetail(message_key,"Not Available").build();
+        }
+
+        return Health.up().withDetail(message_key,"Available").build() ;
+    }
+
+    private Boolean isRunningServiceA() {
+        Boolean isRunning = true;
+        // Logic Skipped
+
+        return isRunning;
+    }
+}
